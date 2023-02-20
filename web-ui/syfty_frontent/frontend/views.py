3bb2d12667271ede87249bd15ae31187862cf4e8
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-20 12:05:35 -0600

    view searches owned by logged in user only

--- web-ui/syfty_frontent/frontend/views.py
+++ web-ui/syfty_frontent/frontend/views.py
@@ -1,16 +1,25 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Search
 from django.urls import reverse
 from django.http import Http404
 
 
 def index(request):
-    latest_search_list = Search.objects.order_by('-id')[:5]
+    if not request.user.is_authenticated:
+        return redirect('login')
+    current_user = request.user
+    latest_search_list = Search.objects.filter(
+        owner=current_user).order_by('-id')[:5]
     context = {
         'latest_search_list': latest_search_list,
     }
     return render(request, 'frontend/index.html', context)
+
+
+def results(request, search_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % search_id)
 
 
 def detail(request, search_id):
@@ -28,11 +37,6 @@ def detail(request, search_id):
 def detail(request, search_id):
     search = get_object_or_404(Search, pk=search_id)
     return render(request, 'frontend/detail.html', {'search': search})
-
-
-def results(request, search_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % search_id)
 
 
 def update(request, search_id):
